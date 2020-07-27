d55b84a878c0889da02b1fcf6340492b914c55d0
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2020-07-27 14:28:36 +0200

    ngIf with else ng-template

--- src/db-data.ts
+++ src/db-data.ts
@@ -1,12 +1,12 @@
 import { Course } from './app/model/course';
-
+//iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
 
 export const COURSES: Course[] = [
-
+undefined,
     {
         id: 1,
         description: "Angular Core Deep Dive",
-        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        
         longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
         category: 'INTERMEDIATE',
         lessonsCount: 10
@@ -79,7 +79,8 @@ export const COURSES: Course[] = [
         iconUrl: "https://s3-us-west-1.amazonaws.com/angular-university/course-images/material_design.png",
         longDescription: "Build Applications with the official Angular Widget Library",
         category: 'ADVANCED'
-    }
+    },
+    undefined
 
 ];
 
