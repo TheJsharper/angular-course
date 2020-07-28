5d6f6e0d36eb1a6492d7bc41630defa268ca43cf
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2020-07-28 10:49:34 +0200

    introducting scss and material desing

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'course-card',
   templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.css']
+  styleUrls: ['./course-card.component.scss']
 })
 export class CourseCardComponent implements OnInit {
 
