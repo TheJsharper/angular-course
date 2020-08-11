e1d3935d6c1e1ae72c155b7b89a2b5ed86a5d505
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2020-08-11 10:37:19 +0200

    output example completed

--- src/app/lessons/basics/outputs/display/app.output-display.component.ts
+++ src/app/lessons/basics/outputs/display/app.output-display.component.ts
@@ -12,26 +12,32 @@ export class AppOutputDisplayComponent {
 
 
         if (i == 1 && this.selectedProperty[0] == "avatar") return this.getImageBackground();
-        if (i == 0) {
-            if (this.selectedProperty[i] == "status") {
-                return { "with": "0.9%" };
-            }
-            return { "flex": 1 };
-        } else {
-            return { "flex": 4 };
+        if (i == 1 && this.selectedProperty[0] == "status") {
+            console.log("====>x")
+            return { "margin-left": "20%" };
         }
+        if (this.selectedProperty[0] != "status")
+            if (i == 0) {
+
+                return { "flex": 1 };
+            } else {
+                return { "flex": 4 };
+            }
 
     }
     getFormatter(item: any, i: number): string {
-        if (item == "dob") {
-            return "Birthday";
-        }
-        if (this.selectedProperty[0] == "avatar" || this.selectedProperty[0] == "status") return "";
-        if (i == 0) {
-            const value: string = item as string;
+        const value: string = item as string;
+        if (item == "dob") return "Birthday";
+
+        if (i == 0 && this.selectedProperty[0] == "avatar") return "Phone";
+        if (i == 1 && this.selectedProperty[0] == "avatar") return "";
+        if (i == 0 && this.selectedProperty[0] != "status") {
+
 
             return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
         }
+        if (i == 0 && this.selectedProperty[0] == "status") return "";
+        if (i == 1 && this.selectedProperty[0] == "status") return `Status`;
 
         return i == 0 ? (<string>item).charAt(0).toUpperCase() + (<string>item).slice(1) : item;
     }
