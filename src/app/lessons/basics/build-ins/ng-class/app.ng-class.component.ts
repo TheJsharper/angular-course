f6a5ac8e965846c9ea83fb8f03513c7dba586ab3
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2020-08-18 15:49:18 +0200

    ui prototypes are onto ng-class component

---
+++ src/app/lessons/basics/build-ins/ng-class/app.ng-class.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+    selector:'ng-class',
+    templateUrl:'./app.ng-class.component.html',
+    styleUrls:["./app.ng-class.component.scss"]
+})
+export class AppNgClassComponent{}
---
+++ src/app/lessons/basics/build-ins/ng-class/app.ng-class.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { AppNgClassComponent } from "./app.ng-class.component";
+
+@NgModule({
+    declarations:[AppNgClassComponent],
+    imports:[],
+    exports:[AppNgClassComponent]
+
+})
+export class AppNgClassModule{}
