4d8231a309c69dcee639585521c62a266b75d95f
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2020-08-20 19:54:47 +0200

    Half pie is developing

--- src/app/lessons/basics/build-ins/ng-class/widgets/pies/half/app.half-pie.component.ts
+++ src/app/lessons/basics/build-ins/ng-class/widgets/pies/half/app.half-pie.component.ts
@@ -10,37 +10,75 @@ import { getRandMinMax } from 'src/db-data';
 export class AppHalfPieComponent implements OnInit {
 
     ngOnInit(): void {
+
+
+        const values: HalfPieValue[] = this.getValues();
+
+        const limited: number = this.getLimited(values);
+
+        const dataProcentages: HalfPieValue[] = this.getDataProcentages(values, limited);
+
+        const dataDegresses: HalfPieDataView[] = this.getHalfPieDataView(dataProcentages);
+
+
+        const totalProcentages: number = this.getTotalProcentages(dataDegresses);
+    
+
+        const totalDegresses: number = this.getTotalDegreeses(dataDegresses) ;
+
+        const twoHalfDataView:SplitDataView = this.getTwoHalfData(dataDegresses, limited);
+
+        console.log("dataProcenatges", dataProcentages, "DataDegreesses", dataDegresses, "limit", limited, "values", values, "total Procentages", totalProcentages, "Toatl Degresses", totalDegresses, "split data", twoHalfDataView);
+
+
+
+    }
+
+    private getValues(): HalfPieValue[] {
         const values: HalfPieValue[] = SELLERS.map((seller: Seller) => {
             return { label: seller.name, count: seller.points, };
         });
-        const limit: number = values.reduce((prev: number, cur: HalfPieValue) => {
+        return values;
+    }
+    private getLimited(data: HalfPieValue[]): number {
+        const limit: number = data.reduce((prev: number, cur: HalfPieValue) => {
             return prev += cur.count;
         }, 0);
+        return limit;
+    }
+
+    private getDataProcentages(values: HalfPieValue[], limited: number): HalfPieValue[] {
         const dataProcentages: HalfPieValue[] = values.map((value: HalfPieValue) => {
-            return { label: value.label, count: value.count / limit * 100 };
+            return { label: value.label, count: value.count / limited * 100 };
         });
-
-        const dataDegresses: HalfPieDataView[] = dataProcentages.map((value: HalfPieValue) => {
-            return { label: value.label, degrees: value.count / 100 * 180, procentage:value.count };
-        });
-        const totalProcentages: number = dataProcentages.reduce((prev: number, cur: HalfPieValue) => { return prev += cur.count }, 0);
-
-        const totalDegresses: number = dataDegresses.reduce((prev: number, cur: HalfPieDataView) => prev += cur.degrees, 0);
-
-        console.log("dataProcenatges", dataProcentages, "DataDegreesses", dataDegresses, "limit", limit, "values", values, "total Procentages", totalProcentages, "Toatl Degresses", totalDegresses);
-
-
+        return dataProcentages;
 
     }
-
-    private getTwoHalfData(data: HalfPieValue[]): { firstHalf: HalfPieValue[], secondHalf: HalfPieValue[] } {
-        const half: number = Math.ceil(data.length);
-        return { firstHalf: data.splice(0, half), secondHalf: data.splice(-half) };
+    private getHalfPieDataView(data: HalfPieValue[]): HalfPieDataView[] {
+        const dataDegresses: HalfPieDataView[] = data.map((value: HalfPieValue) => {
+            return { label: value.label, degrees: value.count / 100 * 180, procentage: value.count };
+        });
+        return dataDegresses;
     }
 
-    private getTwoHalfDataOrderedDegreeses(data: { firstHalf: HalfPieValue[], secondHalf: HalfPieValue[] }): { firstHalf: HalfPieValue[], secondHalf: HalfPieValue[] } {
 
-          //  data.firstHalf.reduce(())
+    private getTotalProcentages(data: HalfPieDataView[]): number {
+        return data.reduce((prev: number, cur: HalfPieDataView) => prev += cur.procentage , 0);
+    }
+
+    private getTotalDegreeses(data: HalfPieDataView[]): number {
+        return data.reduce((prev: number, cur: HalfPieDataView) => prev += cur.degrees, 0)
+    }
+
+    private getTwoHalfData(data: HalfPieDataView[], limited: number): SplitDataView {
+        const half: number = Math.ceil(data.length /2);
+
+        return { firstHalf: data.splice(0, half), secondHalf: data.splice(-half), limited };
+    }
+
+    private getTwoHalfDataOrderedDegreeses(data: SplitDataView): SplitDataView {
+
+        //  data.firstHalf.reduce(())
         return null;
     }
 
@@ -60,8 +98,13 @@ export interface HalfPieValue {
     label: string;
     count: number;
 }
-export interface HalfPieDataView{
-    label:string,
-    procentage:number;
-    degrees:number;
-} 
+export interface HalfPieDataView {
+    label: string,
+    procentage: number;
+    degrees: number;
+}
+export interface SplitDataView {
+    limited: number;
+    firstHalf?: HalfPieDataView[];
+    secondHalf?: HalfPieDataView[];
+}
